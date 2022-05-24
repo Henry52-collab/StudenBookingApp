aaf8301dc912df3dc1eb4035c5eba4a9ab47511b
Henry-GeekOfTheNorth <tanruiheng52@gmail.com>
2022-05-23 21:07:02 -0400

    "Added another activity"

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.widget.EditText;
 import android.view.View;
@@ -16,9 +18,13 @@ public class MainActivity extends AppCompatActivity {
     Button b1,b2;
     EditText ed1,ed2;
     TextView tx1;
+    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        /*
         setContentView(R.layout.activity_main);
         b1 = (Button)findViewById(R.id.button);
         b2 = (Button)findViewById(R.id.button2);
@@ -26,27 +32,27 @@ public class MainActivity extends AppCompatActivity {
         ed2 = (EditText) findViewById(R.id.editTextTextPersonName);
         tx1 = (TextView) findViewById(R.id.textView6);
         tx1.setVisibility(View.GONE);
-        b1.setOnClickListener(new View.OnClickListener(){
-            protected void onClick(){
-                EditText username = (EditText) findViewById(R.id.editTextTextPersonName);
-                EditText password = (EditText) findViewById(R.id.editTextTextPassword);
-                public void login(View view){
-                    if(username.getText().toString().equals("admin") && password.getText().toString().equals("admin123")){
-                        Toast.makeText(getApplicationContext(),"redirecting",Toast.LENGTH_SHORT).show();
-                    }
-                    else{
-                      Toast.makeText(getApplicationContext(),"Wrong credentials",Toast.LENGTH_SHORT).show();
-                    }
-                }
-                public void cancel(View view){}
+        */
+        }
+
+        public void login(View view){
+            /*
+            EditText username = (EditText) findViewById(R.id.editTextTextPersonName);
+            EditText password = (EditText) findViewById(R.id.editTextTextPassword);
+            if(username.getText().toString().equals("admin") && password.getText().toString().equals("admin123")){
+                Toast.makeText(getApplicationContext(),"redirecting",Toast.LENGTH_SHORT).show();
             }
-
-
+            else {
+                Toast.makeText(getApplicationContext(), "Wrong credentials", Toast.LENGTH_SHORT).show();
+            }
+            */
+            Intent intent = new Intent(this,DisplayMessageActivity.class);
+            EditText editText = (EditText) findViewById(R.id.editTextTextPersonName);
+            String message = editText.getText().toString();
+            intent.putExtra(EXTRA_MESSAGE,message);
+            startActivity(intent);
         };
 
-
-
-
-
-    }
+        public void cancel(View view){}
 }
+
