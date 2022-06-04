52419d9dd604ca172776e04e4b373cca81d7a8ef
Henry-GeekOfTheNorth <tanruiheng52@gmail.com>
2022-06-04 13:13:21 -0400

    "Added incomplete admin functionality and more comments"

--- app/src/main/java/com/example/myapplication/CourseController.java
+++ app/src/main/java/com/example/myapplication/CourseController.java
@@ -3,7 +3,7 @@ package com.example.myapplication;
 import java.util.ArrayList;
 
 /**
- * Course controller class for course as to uphold the MVC design principle. Provides control to a course instance
+ * Course controller class for a course as to uphold the MVC design principle. Provides control to a course instance
  * */
 public class CourseController {
 
@@ -13,18 +13,32 @@ public class CourseController {
         this.course = course;
     }
 
+    /**
+     * This method sets a course name
+     * @param name new name
+     * */
     public void setCourseName(String name){
         course.setName(name);
     }
 
+    /**
+     * This method sets a course code
+     * @param code new code
+     * */
     public void setCourseCode(String code){
         course.setCode(code);
     }
 
+    /**
+     * This method gets the Courscode
+     * */
     public String getCourseCode(){
         return course.getCode();
     }
 
+    /**
+     * This method gets the course name
+     * */
     public String getCourseName(){
         return course.getName();
     }
