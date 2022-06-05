789ced6c1a9df458fc6cc97f722ec5308b684c39
Salina Bee <siriuspilas@gmail.com>
2022-06-05 19:10:42 -0400

    Welcome screen and minor adjustments

--- app/src/main/java/com/example/myapplication/DisplayMessageActivity.java
+++ app/src/main/java/com/example/myapplication/DisplayMessageActivity.java
@@ -8,17 +8,46 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+/**
+ * This class is the Welcome Screen, and corresponds to activity_display_message.xml.
+ * By pressing Continue, the app moves to a home screen based on the user's role.
+ * Progress: Aside from requiring the login page (MainActivity.java) to pass Username2 and Type2 here,
+ * everything else should be fully implemented.
+ */
 public class DisplayMessageActivity extends AppCompatActivity {
     private Button logoutBtn;
-    private TextView usernameTV;
+    private Button continueBtn;
+    private TextView username;
+    private TextView role;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_message);
         logoutBtn = (Button) findViewById(R.id.BtnLogout);
-        usernameTV = findViewById(R.id.idTVUserName);
-        String name = getIntent().getStringExtra("username");
-        usernameTV.setText(name);
+        continueBtn = (Button) findViewById(R.id.continueBtn);
+        username = findViewById(R.id.idTVUserName);
+        role = findViewById(R.id.Role);
+
+        /* Set username and role TextView to the respective username and role of user */
+        Intent intent = getIntent();
+        Bundle extras = intent.getExtras();
+        String name = "";
+        String type = "";
+        try {
+            if (extras.containsKey("Username2")) { // user reached welcome page via login
+                name = intent.getStringExtra("Username2");
+                type = intent.getStringExtra("Type2");
+            } else { // user reached welcome page via register
+                name = intent.getStringExtra("Username");
+                type = intent.getStringExtra("Type");
+            }
+        } catch (NullPointerException e) {
+            System.out.println("Extras is null");
+        }
+
+        username.setText(name);
+        role.setText(type);
 
         logoutBtn.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
@@ -27,10 +56,32 @@ public class DisplayMessageActivity extends AppCompatActivity {
                finish();
             }
         });
-        Intent intent = getIntent();
-        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
-        TextView textView = findViewById(R.id.textView);
-        textView.setText(message);
+
+        String finalType = type;
+        String finalName = name;
+        continueBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent j;
+
+                if (finalType.equals("student")) {
+                    j = new Intent(DisplayMessageActivity.this, studentHome.class);
+                } else if (finalType.equals("instructor")) {
+                    j = new Intent(DisplayMessageActivity.this, instructorHome.class);
+                } else { // admin
+                    j = new Intent(DisplayMessageActivity.this, adminHome.class);
+                }
+
+                j.putExtra("Username", finalName);
+                startActivity(j);
+                finish();
+            }
+
+
+        });
+
+
+
     }
 
 
